e153f659e6d0fd2b4a7fe624935b02f8c7b7ca33
Louis Vallance <louisvallance@hotmail.co.uk>
2023-05-10 19:49:05 +0200

    Create internal_correctSign.m

---
+++ +abd/internal_correctSign.m
@@ -0,0 +1,24 @@
+function [PARAM] = internal_correctSign(PARAM, MODE)
+%   Correct the sign of strength properties.
+%
+%   DO NOT RUN THIS FUNCTION.
+%
+%   Layup Analysis Tool 2.4 Copyright Louis Vallance 2023
+%   Last modified 10-May-2023 10:16:13 UTC
+%
+
+%% - DO NOT EDIT BELOW LINE
+%_______________________________________________________________________
+%%
+
+switch MODE
+    case 1.0
+        % Make negative properties positive
+        badValues = PARAM < 0.0;
+        PARAM(badValues) = abs(PARAM(badValues));
+    case -1.0
+        % Make positive properties negative
+        badValues = PARAM > 0.0;
+        PARAM(badValues) = -PARAM(badValues);
+    otherwise
+end
